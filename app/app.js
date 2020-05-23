6ab5314626cc762ef0bdea504932f77b69377052
ianH92 <ianhdrummond1992@gmail.com>
2020-05-23 08:38:42 -0700

    Initializing project; adding Express.js

---
+++ app/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+// TODO For best results use a reverse proxy cache to improve performance
+app.use('/static', express.static(path.join(__dirname, 'html')))
+app.get('/', (req, res) =>  res.sendFile(path.join(__dirname, 'html/main.html')))
+app.listen(port, () => console.log(`App listening on port ${port}`))
